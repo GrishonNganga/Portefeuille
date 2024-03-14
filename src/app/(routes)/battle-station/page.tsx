9b5254f7c7c72402a68ecab83614f2a85f1c4d32
Grishon Nganga <grishon.nganga01@gmail.com>
2024-03-14 17:45:40 +0300

    Add /battle-station page

--- src/app/(routes)/battle-station/page.tsx
+++ src/app/(routes)/battle-station/page.tsx
@@ -3,6 +3,17 @@ import { motion } from "framer-motion";
 
 import Layout from "@/app/components/ui/layout";
 
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel"
+import Image from "next/image";
+import { CheckCheckIcon } from "lucide-react";
+
+const images = ["/grish-battlestation.jpeg", "/station-one.jpg", "/station-two.jpg"]
 export default function Page() {
     return (
         <div className="">
@@ -21,7 +32,67 @@ export default function Page() {
                         }
                     }}
                 >
+                    <div className="container lg:mt-0 mb-20 flex flex-col items-center">
+                        <div className="text-2xl font-bold tracking-wider">
+                            My current work setup
+                        </div>
+                        <div className="flex flex-col text-lg mt-3 text-sm">
+                            <div className="flex gap-x-2 border-t border-gray-100 py-3 ">
+                                <div className="">
+                                    1.
+                                </div>
+                                2021 14" Macbook M1 Pro
+                            </div>
+                            <div className="flex gap-x-2 border-t border-gray-100 py-3">
+                                <div className="">
+                                    2.
+                                </div>
+                                27" HP M27f Display
+                            </div>
+                            <div className="flex gap-x-2 border-t border-gray-100 py-3">
+                                <div className="">
+                                    3.
+                                </div>
+                                1st Gen Airpods Pro
+                            </div>
+                            <div className="flex gap-x-2 border-t border-gray-100 py-3">
+                                <div className="">
+                                    4.
+                                </div>
+                                Noise Cancelling Earbuds
+                            </div>
+                            <div className="flex gap-x-2 border-t border-gray-100 py-3">
+                                <div className="">
+                                    5.
+                                </div>
+                                Notebook
+                            </div>
+                            <div className="flex gap-x-2 border-t border-gray-100 py-3">
+                            <div className="">
+                                    6.
+                                </div>
+                                Pen
+                            </div>
+                        </div>
+                        <div className="lg:w-1/2">
+                            <Carousel className="w-full">
+                                <CarouselContent className="w-full h-full">
+                                    {
+                                        images.map((image, idx) => {
+                                            return (
+                                                <CarouselItem key={idx} className="w-full h-full max-h-max aspect-16/9">
+                                                    <Image src={image} width={500} height={500} alt="My work setup 1" className="w-full h-full object-cover" />
+                                                </CarouselItem>
+                                            )
+                                        })
+                                    }
+                                </CarouselContent>
+                                <CarouselPrevious />
+                                <CarouselNext />
+                            </Carousel>
 
+                        </div>
+                    </div>
                 </motion.div>
             </Layout>
         </div>
