3176438aeb0f7cdbe97af69d20459b56f9531cb3
Grishon Nganga <grishon.nganga01@gmail.com>
2024-04-16 14:47:49 +0300

    fix: add verce.json to see if posts folder will be copied in vercel deployment

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -19,15 +19,13 @@ export function getPostBySlug(slug: string) {
     const { data, content } = matter(fileContents);
     return { ...data, slug: realSlug, content } as IPost;
   }
-  console.log("S", slug)
-  return 
 }
 
-export function getAllPosts(){
+export function getAllPosts(): IPost[] {
   const slugs = getPostSlugs();
   const posts = slugs
     .map((slug) => getPostBySlug(slug))
-    .filter(post => post)
+    .filter((post): post is IPost => !!post)
     .sort((post1, post2) => (post1!.date > post2!.date ? -1 : 1));
   console.log("PS", posts)
   return posts;
