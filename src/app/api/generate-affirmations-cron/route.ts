f5e5c458f84bf4d2eb9188ecc388375305bcd77d
Grishon Nganga <grishon.nganga01@gmail.com>
2024-04-19 11:12:17 +0300

    fix: add fix trying to debug why nextjs cron is not running.

--- src/app/api/generate-affirmations-cron/route.ts
+++ src/app/api/generate-affirmations-cron/route.ts
@@ -1,7 +1,11 @@
 import { generateWeeklyAffirmations } from "@/lib/api/affirmations";
+import { NextResponse } from "next/server";
 
 
 export async function GET(request: Request) {
+    if (request.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+        return NextResponse.json({ status: 401 })
+    }
     await generateWeeklyAffirmations()
-    return new Response(`success`);
+    return NextResponse.json({ ok: true });
 }
--- src/lib/api/affirmations.ts
+++ src/lib/api/affirmations.ts
@@ -45,5 +45,5 @@ export const generateWeeklyAffirmations = async () => {
     //         }
     //     }
     // })
-    console.log("CALLED")
+    console.log("CRON Triggered")
 }
