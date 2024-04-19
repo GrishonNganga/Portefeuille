a384822626c597b9022f943a11d6667556fad2d5
Grishon Nganga <grishon.nganga01@gmail.com>
2024-04-19 11:40:44 +0300

    refactor: remove vercel's cronjobs, move the job to cron-jon.org

--- src/app/api/generate-affirmations-cron/route.ts
+++ src/app/api/generate-affirmations-cron/route.ts
@@ -3,9 +3,7 @@ import { NextResponse } from "next/server";
 
 
 export async function GET(request: Request) {
-    if (request.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
-        return NextResponse.json({ status: 401 })
-    }
+    
     await generateWeeklyAffirmations()
     return NextResponse.json({ ok: true });
 }
